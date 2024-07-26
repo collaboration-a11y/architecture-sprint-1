b14dfa82211bcc3ab4eb6ce5ef5f9a58d6beb2cc
collaboration-a11y <newfaceof+1@yandex.ru>
2024-07-26 10:17:51 +0300

    fix: return microfrontend folder

--- frontend/src/components/InfoTooltip.js
+++ frontend/src/components/InfoTooltip.js
@@ -1,33 +1,20 @@
-import React from "react";
-import SuccessIcon from "../images/success-icon.svg";
-import ErrorIcon from "../images/error-icon.svg";
-
-const contentByStatus = {
-  success: {
-    icon: SuccessIcon,
-    text: "Вы успешно зарегистрировались",
-  },
-  fail: {
-    icon: ErrorIcon,
-    text: "Что-то пошло не так! Попробуйте ещё раз.",
-  },
-};
+import React from 'react';
+import SuccessIcon from '../images/success-icon.svg';
+import ErrorIcon from '../images/error-icon.svg';
 
 function InfoTooltip({ isOpen, onClose, status }) {
-  const { icon, text } = contentByStatus[status];
+  const icon = status === 'success' ? SuccessIcon : ErrorIcon
+  const text = status === 'success' ? "Вы успешно зарегистрировались" : 
+     "Что-то пошло не так! Попробуйте ещё раз."
   return (
-    <div className={`popup ${isOpen && "popup_is-opened"}`}>
+    <div className={`popup ${isOpen && 'popup_is-opened'}`}>
       <div className="popup__content">
         <form className="popup__form" noValidate>
-          <button
-            type="button"
-            className="popup__close"
-            onClick={onClose}
-          ></button>
-          <div>
-            <img className="popup__icon" src={icon} alt="" />
-            <p className="popup__status-message">{text}</p>
-          </div>
+          <button type="button" className="popup__close" onClick={onClose}></button>
+            <div>
+              <img className="popup__icon" src={icon} alt=""/>
+              <p className="popup__status-message">{text}</p>
+            </div>
         </form>
       </div>
     </div>
